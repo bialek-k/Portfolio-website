bd3c5b906fcc6281dc8b3a9440ce6417fe51ee56
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2022-03-04 15:22:13 +0100

    save

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 
-export default function Home() {
+import { request } from "../lib/datocms";
+
+const HOMEPAGE_QUERY = `
+query MyQuery {
+  allProjects {
+    title
+  }
+}
+
+`;
+
+export async function getStaticProps() {
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+  });
+
+  console.log(data);
+
+  return {
+    props: { data },
+  };
+}
+
+export default function Home(props) {
+  console.log(props);
   return (
     <>
       <Hero />
