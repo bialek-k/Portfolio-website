a85674002a3f2c3c6b0bac1b644883d6b3a1e91f
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2022-04-26 23:29:35 +0200

    new sendgrid

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -6,29 +6,28 @@ export default async function handler(req, res) {
   const body = JSON.parse(req.body);
 
   console.log(body);
-  // const message = `
-  //   Name: ${body.name}\r\n
-  //   Email: ${body.email}\r\n
-  //   Message: ${body.message}
-  // `;
-
-  // const data = {
-  //   to: "krzysztof.bialek1@gmail.com",
-  //   from: "krzysztof.bialek1@gmail.com",
-  //   subject: `Wiadomość od ${body.name}`,
-  //   text: message,
-  //   html: message.replace(/\r\n/g, "<br>"),
-  // };
+  const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Message: ${body.message}
+  `;
 
   const data = {
     to: "krzysztof.bialek1@gmail.com",
     from: "krzysztof.bialek1@gmail.com",
-    subject: `Wiadomość od ${body.firstName}`,
-    text: body.lastName,
-    html: body.lastName.replace(/\r\n/g, "<br>"),
+    subject: `Wiadomość od ${body.name}`,
+    text: message,
+    html: message.replace(/\r\n/g, "<br>"),
   };
 
-  await mail.send(data);
-
-  res.status(200).json({ status: "ok" });
+  try {
+    await mail.send(data);
+    res.status(200).json({ status: "OK" });
+  } catch (error) {
+    console.log(error);
+    if (error.response) {
+      console.log(error.response.body);
+    }
+    res.status(400).json({ status: "ERROR", message: error.message });
+  }
 }
