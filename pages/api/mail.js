1fe723eb818c04e05dff6cd173e0e92d25c8bf90
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2022-04-26 22:26:56 +0200

    sengrid update

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,25 +1,42 @@
-const mail = require("@sendgrid/mail");
+import { sendEmail } from "../../utils/sendEmail";
 
-mail.setApiKey(process.env.SENDGRID_API_KEY);
+export default async (req, res) => {
+  if (req.method === "POST") {
+    const { name, email } = req.body;
+    await sendEmail({ name, email, message });
+    return res.status(200).end();
+  }
+  return res.status(404).json({
+    error: {
+      code: "not_found",
+      messgae:
+        "The requested endpoint was not found or doesn't support this method.",
+    },
+  });
+};
 
-export default async function handler(req, res) {
-  const body = JSON.parse(req.body);
+// const mail = require("@sendgrid/mail");
 
-  const message = `
-    Name: ${body.name}\r\n
-    Email: ${body.email}\r\n
-    Message: ${body.message}
-  `;
+// mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const data = {
-    to: "krzysztof.bialek1@gmail.com",
-    from: "krzysztof.bialek1@gmail.com",
-    subject: `Wiadomość od ${body.name}`,
-    text: message,
-    html: message.replace(/\r\n/g, "<br>"),
-  };
+// export default async function handler(req, res) {
+//   const body = JSON.parse(req.body);
 
-  await mail.send(data);
+//   const message = `
+//     Name: ${body.name}\r\n
+//     Email: ${body.email}\r\n
+//     Message: ${body.message}
+//   `;
 
-  res.status(200).json({ status: "ok" });
-}
+//   const data = {
+//     to: "krzysztof.bialek1@gmail.com",
+//     from: "krzysztof.bialek1@gmail.com",
+//     subject: `Wiadomość od ${body.name}`,
+//     text: message,
+//     html: message.replace(/\r\n/g, "<br>"),
+//   };
+
+//   await mail.send(data);
+
+//   res.status(200).json({ status: "ok" });
+// }
